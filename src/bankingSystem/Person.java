41880ffa04130cd3a62f65a55ec36dffe760faff
saraalqallaf <sara24620@gmail.com>
2026-09-08 09:43:07 +0300

    Init Person Class

---
+++ src/bankingSystem/Person.java
@@ -0,0 +1,13 @@
+package bankingSystem;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String role;
+
+    public Person(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = role;
+    }
+}
