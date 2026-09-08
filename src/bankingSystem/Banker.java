4a04630259ccbf3778cd9296bc0ca123d7d33b4b
saraalqallaf <sara24620@gmail.com>
2026-09-08 09:52:12 +0300

    init the Banker class

---
+++ src/bankingSystem/Banker.java
@@ -0,0 +1,7 @@
+package bankingSystem;
+
+public class Banker extends Person{
+    public Banker(String firstName, String lastName, String role) {
+        super(firstName, lastName, role);
+    }
+}
