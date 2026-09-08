a33c3935d19651040ebb79335cb1b0dad128db1a
saraalqallaf <sara24620@gmail.com>
2026-09-08 09:49:46 +0300

    creating Cutomer class as child class of Person, and add init code to it

---
+++ src/bankingSystem/Customer.java
@@ -0,0 +1,15 @@
+package bankingSystem;
+
+import java.util.HashMap;
+
+public class Customer extends Person{
+
+    //Customers may have a checking account, a savings account, or both.
+//    <accountType , accountAmount>
+    HashMap<String,Double> accounts = new HashMap<>();
+
+    public Customer(String firstName, String lastName, String role, HashMap<String, Double> accounts) {
+        super(firstName, lastName, role);
+        this.accounts = accounts;
+    }
+}
