e9994834fe3f65a57b2abed1f0d5a7582f6084ed
saraalqallaf <sara24620@gmail.com>
2026-09-08 14:48:09 +0300

    testing login and signup manually in main

--- src/bankingSystem/SignUp.java
+++ src/bankingSystem/SignUp.java
@@ -31,88 +31,101 @@ public class SignUp {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean signupSuccessful = false;
+        while (true) {
+        System.out.println("------Sign up------");
+            System.out.print("Username: ");
+            String username = scanner.nextLine();
 
-        System.out.print("Username: ");
-        String username = scanner.nextLine();
-        System.out.println();
-        try {
-        File file = new File("accounts.txt");
+            try {
+                File file = new File("accounts.txt");
 
-        if (file.exists()) {
-            Scanner fileScanner = new Scanner(file);
+                if (file.exists()) {
+                    Scanner fileScanner = new Scanner(file);
 
-            while (fileScanner.hasNextLine()) {
-                String line = fileScanner.nextLine();
+                    while (fileScanner.hasNextLine()) {
+                        String line = fileScanner.nextLine();
 
-                String existingUsername = line.split(",")[0];
+                        String existingUsername = line.split(",")[0];
 
-                if (existingUsername.equals(username)) {
-                    System.out.println("Username already exists.");
+                        if (existingUsername.equals(username)) {
+                            System.out.println("Username already exists.");
+                            fileScanner.close();
+                            continue;
+//                            return;
+                        }
+                    }
+
                     fileScanner.close();
-                    return;
                 }
+
+                System.out.print("Password: ");
+                String password = scanner.nextLine();
+
+                if (password.length() < 8 ||
+                        !password.matches(".*[A-Z].*") ||
+                        !password.matches(".*[a-z].*") ||
+                        !password.matches(".*[!@#&()–[{}]:;',?/*~$^+=<>].*") ||
+                        !password.matches(".*[0-9].*")) {
+
+                    System.out.println(
+                            "Password must be at least 8 characters " +
+                                    "and contain uppercase, lowercase, a number, and a special character."
+                    );
+
+//                    scanner.close();
+//                    return;
+                    continue;
+                }
+                System.out.print("Confirmed Password: ");
+                String confirmedPassword = scanner.nextLine();
+
+                if (password.equals(confirmedPassword)) {
+                    // Generate salt
+                    byte[] salt = new byte[16];
+                    new SecureRandom().nextBytes(salt);
+
+                    // Hash password
+                    String hashedPassword = hashPassword(password, salt);
+
+                    // Convert salt to String
+                    String saltString =
+                            java.util.HexFormat.of().formatHex(salt);
+
+                    SignUp account = new SignUp(username, hashedPassword);
+
+                    FileWriter writer = new FileWriter("accounts.txt", true);
+
+                    writer.write(
+                            account.username + "," +
+                                    saltString + "," +
+                                    hashedPassword + "," +
+                                    account.role
+                    );
+
+                    writer.write("\n");
+
+
+                    writer.close();
+
+                    signupSuccessful = true;
+                    System.out.println("Your account has been created!");
+
+                } else {
+                    System.out.println("Passwords do not match");
+                    continue;
+                }
+
+            } catch (Exception e) {
+                System.out.println("Error accessing account file.");
+                continue;
             }
 
-            fileScanner.close();
+
+        if (signupSuccessful) {
+            break;
         }
-
-
-            System.out.print("Password: ");
-            String password = scanner.nextLine();
-            System.out.println();
-            if (password.length() < 8 ||
-                    !password.matches(".*[A-Z].*") ||
-                    !password.matches(".*[a-z].*") ||
-                    !password.matches(".*[0-9].*")) {
-
-                System.out.println(
-                        "Password must be at least 8 characters " +
-                                "and contain uppercase, lowercase, and a number."
-                );
-
-                scanner.close();
-                return;
-            }
-            System.out.print("confirmed Password: ");
-            String confirmedPassword = scanner.nextLine();
-
-            if (password.equals(confirmedPassword)) {
-                // Generate salt
-                byte[] salt = new byte[16];
-                new SecureRandom().nextBytes(salt);
-
-                // Hash password
-                String hashedPassword = hashPassword(password, salt);
-
-                // Convert salt to String
-                String saltString =
-                        java.util.HexFormat.of().formatHex(salt);
-
-                SignUp account = new SignUp(username, hashedPassword);
-
-                FileWriter writer = new FileWriter("accounts.txt", true);
-
-                writer.write(
-                        account.username + "," +
-                                saltString + "," +
-                                hashedPassword + "," +
-                                account.role
-                );
-
-                writer.write("\n");
-
-                writer.close();
-
-                System.out.println("Your account has been created!");
-
-            } else{
-            System.out.println("Passwords do not match");
         }
-
-        } catch (Exception e) {
-        System.out.println("Error accessing account file.");
-    }
-
-        scanner.close();
+            scanner.close();
     }
 }
